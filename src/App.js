021f418df082925e97a3abe1d8ab24c5b0f49de1
Jenni Lohi <jennilohi@hotmail.com>
2022-03-18 22:46:57 +0200

    valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const URL = 'http://localhost/ostoslista/';
+
+
+
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+  const [amount, setAmount] = useState("");
+
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+
+
+      });
+  }, []);
+
+  function save(e) {
+    e.preventDefault();
+
+    const json = JSON.stringify({ description: item, amount: amount });
+    axios.post(URL + 'add.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        setItems(items => [...items, response.data]);
+        setItem('');
+        setAmount(items => [...items, response.data]);
+        setAmount("");
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }
+
+  function remove(id) {
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + 'delete.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        const newListWithoutRemoved = items.filter((item) => item.id !== id);
+        setItems(newListWithoutRemoved);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save} >
+        <h1>Ostoslista</h1>
+        <label htmlFor="">Täydennä ostoslistaa!</label>
+        <input value={item} placeholder="Lisää uusi tuote" onChange={e => setItem(e.target.value)} />
+        <input value={amount} placeholder="Lisää määrä" onChange={e => setAmount(e.target.value)} />
+        <button>Tallenna</button>
+      </form>
+
+      <ol>
+        {items?.map(item => (
+          <li key={item.id}>{item.description} {item.amount}
+            <a className='delete' onClick={() => remove(item.id)} href='#'> Delete</a></li>
+        ))}
+      </ol>
     </div>
   );
 }
